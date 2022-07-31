c53cfe80fcfb1762856f481b192aac42a5bbf5dd
Athul T <athultvinod01@gmail.com>
2022-07-31 22:21:42 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlumniComponent } from './alumni/alumni.component';
+import { EmployerComponent } from './employer/employer.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:MainComponent},
+  
+  {path:'login', component:LoginComponent},
+  {path:'alumni', component:AlumniComponent},
+  {path:'employer', component:EmployerComponent},
+  {path:'faculty',component:FacultyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { EmployerComponent } from './employer/employer.component';
+import { AlumniComponent } from './alumni/alumni.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainComponent,
+    LoginComponent,
+    FacultyComponent,
+    EmployerComponent,
+    AlumniComponent
   ],
   imports: [
     BrowserModule,
