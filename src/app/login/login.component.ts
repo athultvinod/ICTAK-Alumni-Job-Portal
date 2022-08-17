616ea44d35c7967c186742eb8366bfc573a2bcee
AyshaThesneemNK <Aashaysha2@gmail.com>
2022-08-17 13:53:08 +0300

    signup frontend

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup,Validators,FormBuilder, EmailValidator } from '@angular/forms';
+import { FormControl,FormGroup,Validators,FormBuilder ,EmailValidator} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  registerForm:any = FormGroup;
-  submitted = false;
-  constructor( private formBuilder: FormBuilder) { }
-  get f() { return this.registerForm.controls; }
+    //Form Validables 
+registerForm:any = FormGroup;
+submitted = false;
+constructor( private formBuilder: FormBuilder){}
+//Add user form actions
+get f() { return this.registerForm.controls; }
 onSubmit() {
   
   this.submitted = true;
@@ -17,20 +19,20 @@ onSubmit() {
   if (this.registerForm.invalid) {
       return;
   }
-
   //True if all the fields are filled
-  else{
-    location.replace("http://localhost:4200/jobs")
+  if(this.submitted)
+  {
+    location.replace('/jobs');
   }
+ 
 }
-
-  ngOnInit(): void {
+  ngOnInit() {
     //Add User form validations
     this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-      });
-    }
+     email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+    });
   }
+}
 
 
